f3169a931d801bdee3ed051863707526918f46a5
cmercier2 <clementjrmercier@gmail.com>
2019-05-22 14:26:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm'
 import Header from './components/header/header';
-<<<<<<< HEAD
-import ComputerList from './Containers/ComputerList';
-=======
 import { BrowserRouter as Router, Route } from "react-router-dom";
->>>>>>> develop
 
 export default class App extends React.Component {
 
-<<<<<<< HEAD
-  async componentDidMount() {
-
-    let isSuccess = await userService.login({login : "lolo", password : "coucou" })
-      .catch(err => console.log(err));
-  }
-
-  render () {
-    return (
-      <div className="App">
-        <Header/>
-        <LoginForm/>
-      <ComputerList/>
-=======
   render() {
     return (
       <div className="App">
@@ -31,7 +13,6 @@ export default class App extends React.Component {
             <Route path="/**" component={Header} />
             <Route path="/login" component={LoginForm} />
           </Router>
->>>>>>> develop
       </div>
     );
   }
