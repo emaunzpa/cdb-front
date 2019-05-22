c1f27521ea79496fb5f797907d3f28e435ec5eda
vprotois <vprotois@excilys.com>
2019-05-22 14:12:59 +0200

    Added company and pagination

---
+++ src/components/company/CompanyList.js
@@ -0,0 +1,65 @@
+import React,{Component} from 'react';
+import companyService from '../../services/CompanyService';
+import userService from '../../services/UserService';
+import Pagination from '../pagination';
+
+class CompanyList extends Component {
+
+    state = {
+        companies :[],
+        options: Pagination
+        
+    }
+
+     updateList = async (options) => {
+        console.log("is updating");
+        let isSuccess = await userService.login({login : "lolo", password : "coucou" })
+          .catch(err => console.log(err));
+        if(isSuccess) {
+            console.log(options)
+            this.setState({
+              companies : await companyService.list(options)
+              .catch(err => console.log(err)),
+              size : await companyService.count("")
+              .catch(err => console.log(err))
+              
+          }) 
+        }
+        this.forceUpdate();
+        console.log(this.state.companies)
+        console.log(this.state.size)
+    }
+
+    componentDidMount() {
+        let options = {
+            page : 1,
+            itemPerPage:10
+        }
+        this.updateList(options)
+    }
+
+
+    render(){
+        return (
+            <div className ="container">
+                <div className = "companyList">
+                    <ul>
+                        {
+                            
+                            this.state.companies.map( company =>
+                                <li key ={company.id}>
+                                    Id : {company.id} Name : {company.name}
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
+                <Pagination size={this.state.size} update={(options)=> this.updateList(options)}></Pagination>
+
+            </div>
+        )
+    }
+
+}
+
+export default CompanyList
