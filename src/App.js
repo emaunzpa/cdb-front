f651d80935a1f51bf6c9216819e90060b7c36406
emaunzpa <emaunzparkitny@excilys.com>
2019-05-22 15:15:40 +0200

    Footer - Integration of the footer in app and added routing to footer.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm'
 import Header from './components/header/header';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import UserService from './services/UserService';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-          <Router>
-            <Route path="/**" component={Header} />
-            <Route path="/login" component={LoginForm} />
-          </Router>
+        <Router>
+          <Header />
+          <Route path="/login" component={LoginForm} />
+
+          <Route path="/" exact
+            render={() => (UserService.isAuthenticated() ? (
+              <h1>Vous êtes sur la page des computers</h1>
+            ) : <Redirect to="/login"></Redirect>
+            )} />
+
+          <Route path="/computers"
+            render={() => (UserService.isAuthenticated() ? (
+              <h1>Vous êtes sur la page des computers</h1>
+            ) : <Redirect to="/login"></Redirect>
+            )} />
+
+          <Route path="/companies"
+            render={() => (UserService.isAuthenticated() ? (
+              <h1>Vous êtes sur la page des companies</h1>
+            ) : <Redirect to="/login"></Redirect>
+            )} />
+
+          <Footer />
+        </Router>
       </div>
     );
   }
