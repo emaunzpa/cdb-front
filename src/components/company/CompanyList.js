fd88a4992c3fa7f094dafdc6edf72190162992cd
emaunzpa <emaunzparkitny@excilys.com>
2019-05-28 11:23:17 +0200

    OrderBy - Added oredering for company list and computer list.

--- src/components/company/CompanyList.js
+++ src/components/company/CompanyList.js
@@ -19,7 +19,8 @@ class CompanyList extends Component {
 
     state = {
         companies: [],
-        openSnack: false
+        openSnack: false,
+        reverse: false
     }
 
 
@@ -140,11 +141,12 @@ class CompanyList extends Component {
         })
     }
     orderBy = async (column) => {
-        this.setState({ orderBy: column });
+        this.setState({ orderBy: column, reverse: !this.state.reverse });
         let options = {
             page: 1,
             itemPerPage: 10,
-            orderBy: column
+            orderBy: column,
+            reverse: this.state.reverse
         }
         this.updateList(options)
     }
